2c8467863e99d5ac6fc907da9bea444cf48e2ddc
alamistiqha <alamistiqha@gmail.com>
2019-05-23 12:50:19 +0800

    Firt

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+!function(e)
+{var t={};
+function n(r)
+{if(t[r])return t[r].exports;
+	var 
+	i=t[r]={i:r,l:!1,exports:{}};
+	return e[r].call(i.exports,i,i.exports,n),i.l=!0,i.exports}n.m=e,n.c=t,n
+	.d=function(e,t,r){
+		n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n
+	.r=function(e){
+		"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),
+		Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)
+			return e;
+			if(4&t&&"object"==typeof e&&e&&e.__esModule)
+				return e;
